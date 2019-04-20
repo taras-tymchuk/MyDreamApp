ba3267d7ad83436d7dd8b6ce6c45e16d3ceff1a1
Taras Tymchuk <32062140+taras-tymchuk@users.noreply.github.com>
2019-04-20 10:57:04 +0300

    Added filter field (#2)

---
+++ src/app/customers/customers-list/filter-textboxcomponent.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit } from '@angular/core'
+
+@Component({
+    selector: 'filter-textbox',
+    template: `
+        Filter: <input type="text" />
+    `
+})
+export class FilterTextBoxComponent implements OnInit {
+    
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+    }
+}
